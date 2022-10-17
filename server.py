c8dcb5c01275eb7b66f1d5dd751f44f94e9fa50a
Andrés Court <andres@andrescourt.com>
2022-10-16 21:38:55 -0500

    Passing parameters to the template

--- server.py
+++ server.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
+import random
+from datetime import datetime
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    random_number = random.randint(1, 10)
+
+    copy_year = datetime.now().year
+
+    return render_template('index.html', num=random_number, copy_year=copy_year)
 
 
 if __name__ == "__main__":
