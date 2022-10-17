57f96151631512a62678ea03b0df87f9ae20a980
Andrés Court <andres@andrescourt.com>
2022-10-16 22:04:12 -0500

    Read form api and display in template

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import random
 from datetime import datetime
+import requests
 
 app = Flask(__name__)
 
@@ -14,5 +15,18 @@ def home():
     return render_template('index.html', num=random_number, copy_year=copy_year)
 
 
+@app.route("/guess/<string:name>")
+def guess_route(name: str):
+    res = requests.get(f'https://api.genderize.io?name={name.lower()}')
+    res.raise_for_status()
+    gender = res.json()['gender']
+
+    res = requests.get(f'https://api.agify.io?name={name.lower()}')
+    res.raise_for_status()
+    age = res.json()['age']
+
+    return render_template('guess.html', name=name.title(), gender=gender, age=age)
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
